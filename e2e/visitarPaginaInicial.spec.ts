8d6b2d40873717f5f1ff74856270e718dcf56002
RonaldoCesar28 <ronaldocesar28@outlook.com>
2026-09-14 20:16:50 -0300

    checando se o títuloPassagens está visível

--- e2e/visitarPaginaInicial.spec.ts
+++ e2e/visitarPaginaInicial.spec.ts
@@ -1,8 +1,11 @@
-import test, { expect } from "@playwright/test";
+import { test, expect } from '@playwright/test';
 
 test.describe('Pagina Inicial', () => {
-    test("Deve visitar a pagina inicial", async ({ page }) => {
+    test('Deve visitar a pagina inicial', async ({ page }) => {
         await page.goto('http://localhost:4200/'); // método assincrono de ação/navegação
         await expect(page).toHaveTitle("Jornada Milhas"); // método assincrono de asserção/validação
+
+        const tituloPassagens = page.getByRole('heading', { name: 'Passagens' });
+        await expect(tituloPassagens).toBeVisible();
     });
 });

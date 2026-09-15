87ee6de6364ac23119fcc8315e2da7a3c8496124
RonaldoCesar28 <ronaldocesar28@outlook.com>
2026-09-15 17:30:44 -0300

    interagindo com locators

--- e2e/page-objects/PaginaLogin.ts
+++ e2e/page-objects/PaginaLogin.ts
@@ -1,13 +1,17 @@
-import { Page } from "@playwright/test";
+import { expect, Locator, Page } from "@playwright/test";
 
 export default class PaginaLogin {
     private readonly page: Page;
+    private readonly botaoLogin: Locator;
 
     constructor(page: Page) {
         this.page = page;
+        this.botaoLogin = page.getByTestId('botao-login');
     }
 
     async visitar() {
         await this.page.goto('/');
+        await this.botaoLogin.click();
+        await expect(this.page).toHaveURL('/auth/login');
     }
 }

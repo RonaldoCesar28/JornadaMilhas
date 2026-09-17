1d87db0e4d029ebb71e3641e7bc08fcaa6d40dac
RonaldoCesar28 <ronaldocesar28@outlook.com>
2026-09-17 10:40:00 -0300

    utilizando fixtures

--- e2e/fazerLogin.spec.ts
+++ e2e/fazerLogin.spec.ts
@@ -1,19 +1,12 @@
-import test from "@playwright/test";
-import PaginaLogin from "./page-objects/PaginaLogin";
+import { test } from './page-objects/PaginaLogin';
 
 test.describe('Página de Login', () => {
-    test('Deve fazer login com e-mail e senha válidos', async ({ page }) => {
-        const paginaLogin = new PaginaLogin(page);
-
-        await paginaLogin.visitar();
+    test('Deve fazer login com e-mail e senha válidos', async ({ paginaLogin }) => {
         await paginaLogin.fazerLogin('ronaldo28@gmail.com', 'Senha12345');
         await paginaLogin.loginFeitoComSucesso();
     });
 
-    test('Não deve fazer login com email inválido', async ({ page }) => {
-        const paginaLogin = new PaginaLogin(page);
-
-        await paginaLogin.visitar();
+    test('Não deve fazer login com email inválido', async ({ paginaLogin }) => {
         await paginaLogin.fazerLogin('ronaldo40@gmail.com', 'Senha12345');
         await paginaLogin.mensagemDeErro('Você não está autorizado a acessar este recurso');
     });
